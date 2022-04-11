8646ddab2179ead6863971f6184920954de8260f
ned-park <87483870+ned-park@users.noreply.github.com>
2022-04-11 11:13:05 -0400

    fixed carousel timeout issues

--- js/main.js
+++ js/main.js
@@ -1,52 +1,56 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions
- //and place them in the DOM
+//and place them in the DOM
 
- document.querySelector('#submit').addEventListener('click', getCocktail)
- document.querySelector('#right').addEventListener('click', () => cycle(1))
- document.querySelector('#left').addEventListener('click', () => cycle(-1))
- 
- let current = 0
- let max = 0
- let cache
- 
- function cycle(direction){
-   current = (current + direction) % max
-   current = current < 0? current + max : current
- 
-   display()
-   //Make automatic carousel
-   setTimeout(() => cycle(direction), 5000)
- }
- 
- function display() {
-   let drink = cache.drinks[current]
-   document.querySelectorAll('.hidden').forEach(e => e.classList.toggle('hidden'))
-   document.querySelector('#name').innerText = drink.strDrink
-   document.querySelector('#instructions').innerText = drink.strInstructions
-   document.querySelector('img').src = drink.strDrinkThumb
-   document.querySelector('img').alt = drink.strDrink
- 
-   let ingredients = []
-   for (let i = 1; i < 20; i++) {
-     let ingredient = drink[`strIngredient${i}`]
-     if (ingredient === null || ingredient === '') 
-       break
-     ingredients.push(ingredient)
-   }
-   document.querySelector('#ingredients').innerHTML = ingredients.reduce((str, i) => `${str}<li>${i}</li>`, '')
- }
- 
- function getCocktail() {
-   let name = document.querySelector('input').value
-   fetch(`https:///www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`)
-   .then(res => res.json())
-   .then(data => {
-     current = 0
-     max = data.drinks.length
-     cache = data
-     display()
-   })
-   .catch(err => {
-     console.log(err)
-   })
- }
+document.querySelector('#submit').addEventListener('click', getCocktail)
+document.querySelector('#right').addEventListener('click', () => cycle(1))
+document.querySelector('#left').addEventListener('click', () => cycle(-1))
+
+let current = 0
+let cache
+const timeouts = []
+
+function cycle(direction) {
+  let max = cache.drinks.length
+  current = (current + direction) % max
+  current = current < 0 ? current + max : current
+
+  //only one timeout running at a time
+  for (let i = 0; i < timeouts.length; i++) {
+    clearTimeout(timeouts[i]);
+  }
+  timeouts.push(setTimeout(() => cycle(direction), 3000))
+  display()
+}
+
+function display() {
+  let drink = cache.drinks[current]
+  document.querySelectorAll('.hidden').forEach(e => e.classList.toggle('hidden'))
+  document.querySelector('#name').innerText = drink.strDrink
+  document.querySelector('#instructions').innerText = drink.strInstructions
+  document.querySelector('img').src = drink.strDrinkThumb
+  document.querySelector('img').alt = drink.strDrink
+
+  let ingredients = []
+  for (let i = 1; i < 20; i++) {
+    let ingredient = drink[`strIngredient${i}`]
+    if (ingredient === null || ingredient === '')
+      break
+    ingredients.push(ingredient)
+  }
+  document.querySelector('#ingredients').innerHTML = ingredients.reduce((str, i) => `${str}<li>${i}</li>`, '')
+}
+
+function getCocktail() {
+  let name = document.querySelector('input').value
+  fetch(`https:///www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`)
+    .then(res => res.json())
+    .then(data => {
+      current = 0
+      cache = data
+      running = false
+      display()
+    })
+    .catch(err => {
+      console.log(err)
+    })
+}
